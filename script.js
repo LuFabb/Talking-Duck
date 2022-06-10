46917741620a01e4b0ea3aee6a0163e1c3d7df04
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-10 16:45:49 -0500

    start with the js logic, collect the elements i need

---
+++ script.js
@@ -0,0 +1,5 @@
+// collect the elements i need from my page
+const textArea = document.querySelector('textarea');
+const playButton = document.querySelector('button');
+const pitchBar = document.querySelector('input');
+const duckFigure = document.querySelector('figure');
