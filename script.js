04ec27437b82caa828f50a2609a63e0a35505555
Luca Fabbozzo <lucafabbozzo@gmail.com>
2022-06-10 16:51:52 -0500

    do not compute spaces before and after the sentence whit trim()

--- script.js
+++ script.js
@@ -3,3 +3,9 @@ const textArea = document.querySelector('textarea');
 const playButton = document.querySelector('button');
 const pitchBar = document.querySelector('input');
 const duckFigure = document.querySelector('figure');
+
+// if someone click the button, do what i tell you
+playButton.addEventListener('click', function(){
+  const textLength = textArea.value.trim().length;
+  console.log(textLength)
+})
